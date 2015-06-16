50b1848946a8a62e469c506410a05174838195cc
Nielson-Casey <chif346@gmail.com>
2015-06-15 20:23:31 -0600

    memory

---
+++ house/Memory.java
@@ -0,0 +1,23 @@
+package house;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Memory {
+
+	//Declaring Variables 
+	int five = 5;
+	
+	String yes = "yes";
+	
+	double [] numbers = {1,2,3,4,5};
+	
+	double [][] num = new double[5][3] ;
+		
+	HashMap<String ,String > map = new HashMap<String ,String>();
+	
+	ArrayList<String> list = new ArrayList<String>();
+	
+	
+	
+}
