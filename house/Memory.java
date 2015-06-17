eb3480fbc7103e6e54f4954f8fbc7cf977ece87a
Nielson-Casey <chif346@gmail.com>
2015-06-16 19:27:44 -0600

    Logic

--- house/Memory.java
+++ house/Memory.java
@@ -14,10 +14,52 @@ public class Memory {
 	
 	double [][] num = new double[5][3] ;
 		
-	HashMap<String ,String > map = new HashMap<String ,String>();
+	HashMap<String ,String > map = new HashMap<String,String>();
 	
 	ArrayList<String> list = new ArrayList<String>();
 	
+	// Logic
+	public Memory(){};
 	
+	public int logic()
+	{
+		
+		
+		
+		switch (yes) 
+		{
+		case "no":	
+			System.out.println("Something went wrong!");
+		    break;
+		case "maybe":
+			System.out.println("Something went wrong!");
+			break;
+		case "yes" :
+			System.out.println("Something went right!");
+			break;
+		default:
+			System.out.println("Something went wrong!");
+			break;
+		}
+		
+	
+		for (int i = 0; five > i; i++)
+		{
+			System.out.println("I can count to " + i);
+		}
+		
+		while(five > 6)
+		{
+			System.out.println("I count the same number five " + five + " times.");
+		}
+		
+		return five;
+	}
+
+	
+	
+
+
 	
 }
+
