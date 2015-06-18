c833507819c11a23bc0b143361d61abc7ede1ad7
Nielson-Casey <chif346@gmail.com>
2015-06-17 20:55:41 -0600

    border

--- house/javaGUI.java
+++ house/javaGUI.java
@@ -1,25 +1,49 @@
 package house;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class javaGUI {
+
+public class javaGUI extends JFrame{
 
 	public static void main(String[] args) {
 		
+		JButton jbtCtn = new JButton("Center");
+		JButton jbtNo = new JButton("North");
+		JButton jbtSo = new JButton("South");
+		JButton jbtEa = new JButton("East");
+		JButton jbtWe = new JButton("West");
 		
+		ImageIcon img = new ImageIcon("C:\\Users\\computer\\Desktop\\luna.png");
+		
+		
+		//BorderLayout border = new BorderLayout(5,10);
+		
+		
+		JPanel pnlinput = new JPanel();
+
+		pnlinput.setLayout(new BorderLayout(3, 2));
 		
 		JFrame frame = new JFrame("My Window");
 		
-		JButton jbtOK = new JButton("OK");
-		ImageIcon img = new ImageIcon("C:\\Users\\computer\\Desktop\\luna.png");
-	
 		
-		frame.add(jbtOK);
+		
+		frame.add(jbtCtn);
+		frame.add(jbtNo);
+		frame.add(jbtSo);
+		frame.add(jbtEa);
+		frame.add(jbtWe);
+		
+		//frame.BorderLayout.EAST;
+		
 		frame.setIconImage(img.getImage());
 		
-		
+		frame.pack();
 		frame.setSize(400, 300);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
